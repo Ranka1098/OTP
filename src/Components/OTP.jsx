d0051f3fd626016d4b09a2377fab148a48b25925
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2025-04-09 23:05:40 +0530

    get otp filed last value using slice method

--- src/Components/OTP.jsx
+++ src/Components/OTP.jsx
@@ -8,10 +8,26 @@ const OTP = () => {
     new Array(Otp_Digit_Count).fill("")
   );
 
+  const handleChange = (e, index) => {
+    const value = e.target.value;
+
+    // create shallow copy
+    const copyOtp = [...otpFields];
+    copyOtp[index] = value.slice(-1);
+    setOtpFields(copyOtp);
+  };
+
   return (
     <div className="container ">
       {otpFields.map((value, index) => {
-        return <input type="text" key={index} />;
+        return (
+          <input
+            type="text"
+            key={index}
+            value={otpFields[index]}
+            onChange={(e) => handleChange(e, index)}
+          />
+        );
       })}
     </div>
   );
